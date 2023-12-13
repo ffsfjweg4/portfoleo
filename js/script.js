48299dbe0146d8ad626ec54f1a3ce18729981a5b
ffsfjweg4 <153703825+ffsfjweg4@users.noreply.github.com>
2023-12-13 10:35:13 +0500

    Add files via upload

---
+++ js/script.js
@@ -0,0 +1,10 @@
+ alert("Привет, котик!");
+ let num1=parseInt(prompt("Введение первое число"));
+ let num2=parseInt(prompt("Введение второе число"));
+ sum();
+ multiplication();
+ division();
+ subtraction();
+ function sum() {
+ 	alert('Результат сложения: ${num1+num2}');
+ }
